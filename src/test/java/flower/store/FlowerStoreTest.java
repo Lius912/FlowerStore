3ed2693f8b7e73b985ae3b0077d868ffad496efd
Lev Lysyk <levlisik@gmail.com>
2024-10-13 21:33:06 +0300

    fixed linting erorrs

--- src/test/java/flower/store/FlowerStoreTest.java
+++ src/test/java/flower/store/FlowerStoreTest.java
@@ -7,20 +7,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FlowerStoreTest {
+    private double price = 80;
+    private double priceTwo = 100;
+    private double length = 8.0;
+    private double lengthTwo = 9.0;
+
+
+
     @Test
     public void testSearch() {
-        Flower flower = new Flower(8.0, FlowerColor.GREEN, 80, FlowerType.CHAMOMILE);
-        Flower flower2 = new Flower(8.0, FlowerColor.BLUE, 80, FlowerType.CHAMOMILE);
-        Flower flower3 = new Flower(8.0, FlowerColor.GREEN, 100, FlowerType.CHAMOMILE);
-        Flower flower4 = new Flower(9.0, FlowerColor.GREEN, 80, FlowerType.CHAMOMILE);
+        Flower flower = new Flower(length
+        , FlowerColor.GREEN, price, FlowerType.CHAMOMILE);
+        Flower flowerTwo = new Flower(length
+        , FlowerColor.BLUE, price, FlowerType.CHAMOMILE);
+        Flower flowerThree = new Flower(length
+        , FlowerColor.GREEN, priceTwo, FlowerType.CHAMOMILE);
+        Flower flowerFour = new Flower(lengthTwo
+        , FlowerColor.GREEN, price, FlowerType.CHAMOMILE);
         Store store = new Store();
         store.addFlower(flower);
-        store.addFlower(flower2);
-        store.addFlower(flower3);
-        store.addFlower(flower4);
+        store.addFlower(flowerTwo);
+        store.addFlower(flowerThree);
+        store.addFlower(flowerFour);
         List<Flower> expectedMatchedFlowers = new ArrayList<>();
         expectedMatchedFlowers.add(flower);
-        expectedMatchedFlowers.add(flower3);
+        expectedMatchedFlowers.add(flowerThree);
         List<Flower> matched = store.search(flower);
         Assertions.assertEquals(expectedMatchedFlowers.get(0), matched.get(0));
         Assertions.assertEquals(expectedMatchedFlowers.get(1), matched.get(1));
